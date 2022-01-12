5ab160ba33024896324def31d6313722bf7f562c
excelitai-tusher <tusher@excelitai.com>
2022-01-12 14:52:53 +0600

    first

--- lib/order/body.dart
+++ lib/order/body.dart
@@ -18,61 +18,104 @@ class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 3,
-        itemBuilder: (context, index) => Category(index),
-      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 3,
+            itemBuilder: (context, index) => Category(index),
+          ),
+          ),
+        ],
       ),
     );
   }
 
    Widget Category(int index) {
-     return SingleChildScrollView(
-       child: Column(
-         crossAxisAlignment: CrossAxisAlignment.start,
-         children: [
-           Padding(
-             padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-             child: GestureDetector(
-               onTap: () {
-                 setState(() {
-                   selectedIndex = index;
-                 });
-               },
-               child: Column(
-                 //crossAxisAlignment: CrossAxisAlignment.start,
-                 children: [
-                   SizedBox(height: 20,),
-                   Text(
-                     category[index],
-                     style: TextStyle(
-                       color: selectedIndex == index ? kTextColor : kTextLightColor,
-                       fontWeight: FontWeight.bold,
-                       fontSize: 18,
-                     ),
+     return Column(
+       crossAxisAlignment: CrossAxisAlignment.start,
+       children: [
+         Padding(
+           padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+           child: GestureDetector(
+             onTap: () {
+               setState(() {
+                 selectedIndex = index;
+               });
+             },
+             child: Column(
+               //crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 SizedBox(height: 20,),
+                 Text(
+                   category[index],
+                   style: TextStyle(
+                     color: selectedIndex == index ? kTextColor : kTextLightColor,
+                     fontWeight: FontWeight.bold,
+                     fontSize: 18,
                    ),
-                   Container(
-                     margin: EdgeInsets.only(top: kDefaultPaddin),
-                     height: 2,
-                     width: 30,
-                     color: selectedIndex == index ? Colors.black : Colors.transparent,
-                   ),
-
-                   SizedBox(height: 20,),
-                    ListView.builder(
-                        itemBuilder: (BuildContext context, int index) {
-                        }
-                    ),
-                 ],
-               ),
+                 ),
+                 Container(
+                   margin: EdgeInsets.only(top: kDefaultPaddin),
+                   height: 2,
+                   width: 30,
+                   color: selectedIndex == index ? Colors.black : Colors.transparent,
+                 ),
+               ],
              ),
            ),
-         ],
-       ),
+         ),
+         SizedBox(
+           height: 180,
+           child: ListView.separated(
+             separatorBuilder: (context, index) {
+               return SizedBox(
+                 width: 15,
+               );
+             },
+             shrinkWrap: true,
+             scrollDirection: Axis.horizontal,
+             itemCount: Item.list.length,
+             itemBuilder: (BuildContext context, i) {
+               Item product= Item.list[i];
+               return Column(
+                 children: [
+                   SizedBox(height: 10,),
+                   ListView(
+                     children: [
+                       Card(
+                           child: ListTile(
+                             title:Text(names[i]) ,
+                           )
+                       ),
+                       Card(
+                         child: ListTile(
+                           title: Text(names[i]),
+                         ),
+                       ),
+                       Card(
+                           child: ListTile(
+                             title: Text(names[i]),
+                           )
+                       ),
+                     ],
+                     shrinkWrap: true,
+                     reverse: true,
+                     padding: EdgeInsets.all(10),
+                   ),
+                   SizedBox(height: 10,)
+                 ],
+               );
+             },
+           ),
+         )
+       ],
      );
+
+
    }
+
 }
