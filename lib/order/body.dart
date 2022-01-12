c4652bf6d30a87236f2c7e5cf2ef1e77839ce332
excelitai-tusher <tusher@excelitai.com>
2022-01-12 16:30:05 +0600

    first

--- lib/order/body.dart
+++ lib/order/body.dart
@@ -1,3 +1,4 @@
+
 import 'package:classy_ui_design/constant.dart';
 import 'package:classy_ui_design/model/item%20description.dart';
 import 'package:flutter/cupertino.dart';
@@ -21,7 +22,7 @@ class _BodyState extends State<Body> {
       child: Column(
         children: [
           SizedBox(
-            height: MediaQuery.of(context).size.height,
+            height: MediaQuery.of(context).size.height/ 9,
             width: MediaQuery.of(context).size.width,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
@@ -29,6 +30,8 @@ class _BodyState extends State<Body> {
             itemBuilder: (context, index) => Category(index),
           ),
           ),
+
+          item(),
         ],
       ),
     );
@@ -68,54 +71,73 @@ class _BodyState extends State<Body> {
              ),
            ),
          ),
-         SizedBox(
-           height: 180,
-           child: ListView.separated(
-             separatorBuilder: (context, index) {
-               return SizedBox(
-                 width: 15,
-               );
-             },
-             shrinkWrap: true,
-             scrollDirection: Axis.horizontal,
-             itemCount: Item.list.length,
-             itemBuilder: (BuildContext context, i) {
-               Item product= Item.list[i];
-               return Column(
-                 children: [
-                   SizedBox(height: 10,),
-                   ListView(
-                     children: [
-                       Card(
-                           child: ListTile(
-                             title:Text(names[i]) ,
-                           )
-                       ),
-                       Card(
-                         child: ListTile(
-                           title: Text(names[i]),
-                         ),
-                       ),
-                       Card(
-                           child: ListTile(
-                             title: Text(names[i]),
-                           )
-                       ),
-                     ],
-                     shrinkWrap: true,
-                     reverse: true,
-                     padding: EdgeInsets.all(10),
-                   ),
-                   SizedBox(height: 10,)
-                 ],
-               );
-             },
-           ),
-         )
        ],
      );
-
-
    }
 
+
+}
+
+Widget item(){
+  List names = [Item.list,];
+
+  return SizedBox(
+    height: 500,
+    child: ListView.separated(
+      separatorBuilder: (context, index) {
+        return SizedBox(
+          height: 300,
+          width: 200,
+          child: Column(
+            children: [
+              SizedBox(height: 10,),
+              Container(
+                height: 160,
+                width: 360,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 3,
+                      offset: Offset(0, 1), // changes position of shadow
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      ListView(
+                        children: [
+                          Card(
+                              child: ListTile(
+                                title:Text(names.toString()) ,
+                              )
+                          ),
+                        ],
+                        shrinkWrap: true,
+                        reverse: true,
+                        padding: EdgeInsets.all(10),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+      shrinkWrap: true,
+      //scrollDirection: Axis.horizontal,
+      itemCount: Item.list.length,
+      itemBuilder: (BuildContext context, i) {
+        Item product= Item.list[i];
+        return  item();
+      },
+    ),
+  );
 }
